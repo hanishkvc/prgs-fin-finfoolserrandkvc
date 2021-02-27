b5da1b5fea12adfdf9dd0e265526efdd3a9c36bd
hanishkvc <hanishkvc@gmail.com>
2021-02-27 19:39:32 +0530

    document funcs, rename things a bit, continue

--- hkvc-mfutils.py
+++ hkvc-mfutils.py
@@ -12,7 +12,15 @@ gBaseURL = "http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt={
 gCal = calendar.Calendar()
 
 
-def proc_days(start, end, func):
+def proc_days(start, end, handle_date_func):
+    """
+    call the passed function for each date with the given start and end range.
+        The date will be passed to the passed function as year, month, date
+        as integers.
+
+    start and end need to be dictionaries {'y': year_int, 'm': month_int}
+        month_int is from 1 to 12
+    """
     for y in range(start['y'], end['y']+1):
         for m in range(1,13):
             if (y == start['y']) and (m < start['m']):
@@ -24,21 +32,27 @@ def proc_days(start, end, func):
                 if d == 0:
                     continue
                 print(" %d "%(d), end="")
-                func(y,m,d)
+                handle_date_func(y,m,d)
 
 
-def handle_date(y, m, d):
+def fetch4date(y, m, d):
+    """
+    the default handler function passed to proc_days.
+
+    One can call this directly by passing the year, month and date one is interested in.
+        month should be one of 1 to 12
+    """
     print(y,m,d)
     url = gBaseURL.format(d,calendar.month_name[m][:3],y)
     print(url)
-    cmd = "wget {} --output-document=data/{}{:02}{:02}.csv".format(url,y,m,d)
+    cmd = "wget {} --continue --output-document=data/{}{:02}{:02}.csv".format(url,y,m,d)
     os.system(cmd)
 
 
 if len(sys.argv) > 1:
     start = { 'y': int(sys.argv[1]), 'm': 1 }
     end = { 'y': int(sys.argv[2]), 'm': 12 }
-    proc_days(start, end, handle_date)
+    proc_days(start, end, fetch4date)
 else:
     bQuit = False
     while not bQuit:
