c1c26a590c278d9603ebcf9bdf7b03e3f0fcb39f
hanishkvc <hanishkvc@gmail.com>
2021-02-27 10:32:13 +0530

    Initial commit: Generate days over given year range
    uses calendar module to generate the days over the given year range

---
+++ hkvc-mfutils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Get and work with MF nav data
+# HanishKVC, 2021
+
+import sys
+import calendar
+
+#https://www.amfiindia.com/spages/NAVAll.txt?t=27022021
+gBaseURL = "https://www.amfiindia.com/spages/NAVAll.txt?t={}"
+gCal = calendar.Calendar()
+
+def get_days(start, end):
+    for y in range(start['y'], end['y']+1):
+        for m in range(1,13):
+            if (y == start['y']) and (m < start['m']):
+                continue
+            if (y == end['y']) and (m > end['m']):
+                continue
+            for d in gCal.itermonthdays(y,m):
+                if d == 0:
+                    continue
+                print(d)
+
+start = {}
+start['y'] = int(sys.argv[1])
+start['m'] = 1
+end = {}
+end['y'] = int(sys.argv[2])
+end['m'] = 12
+get_days(start, end)
+
