b70593da392c7f6f157de3b6da4350a7c7a47f9e
hanishkvc <hanishkvc@gmail.com>
2021-03-08 11:40:25 +0530

    TabComplete only if user is entering first word
    Check for num of tokens < 1, means user has not yet entered anything
    
    Check for numTokens == 1, would trigger when either user has entered
    one full token or the is in the process of entering the 1st token.
    
    It will also trigger if user is about to start on entering the 2nd
    token, i.e by putting a space after the 1st token.
    
    The check for text != "" ensures that the logic doesnt trigger if
    user as conveyed his intention to enter the 2nd token by placing a
    space after the first token.

--- tabcomplete.py
+++ tabcomplete.py
@@ -12,8 +12,15 @@ L1 = [ "load_data", "fetch_data", "lookat_data", "search_data",
 
 
 def complete(text, state):
-    match = [x for x in L1 if x.startswith(text)]
-    return match[state]
+    """
+    Handle the tab completion
+    """
+    curTokens = readline.get_line_buffer().split()
+    if (len(curTokens) < 1) or ((len(curTokens) == 1) and (text != "")):
+        match = [x for x in L1 if x.startswith(text)]
+        return match[state]
+
+    return None
 
 
 readline.parse_and_bind("tab: complete")
