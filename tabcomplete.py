f5147e6c4dcfd380974e29f669d266fca57e2b23
hanishkvc <hanishkvc@gmail.com>
2021-03-08 11:31:33 +0530

    Tab complete from top level user easy use functions
    However this doesnt bother if the user is at the begining or some
    where in the middle of the line, they are entering.

--- tabcomplete.py
+++ tabcomplete.py
@@ -4,5 +4,18 @@
 
 import readline
 
+
+L1 = [ "load_data", "fetch_data", "lookat_data", "search_data",
+        "show_plot", "mftypes_list",
+        "quit"
+        ]
+
+
+def complete(text, state):
+    match = [x for x in L1 if x.startswith(text)]
+    return match[state]
+
+
 readline.parse_and_bind("tab: complete")
+readline.set_completer(complete)
 
