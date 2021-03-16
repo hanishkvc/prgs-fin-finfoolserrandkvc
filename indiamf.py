7d905af6a16aeca0704f2628ef15560616fbca2d
hanishkvc <hanishkvc@gmail.com>
2021-03-16 20:20:32 +0530

    BasicSkeleton to parse whenFetched simplerLoad
    Idea is to parse the data csv when its fetched and then store
    the data into a pickle, without filtering anything out.
    
    Inturn when load4date is called, load from this pickle and filter
    out as required.
    
    The idea is that hopefully chances are this should be faster.

--- indiamf.py
+++ indiamf.py
@@ -122,12 +122,93 @@ def parse_csv(sFile):
     tFile.close()
 
 
-def fetch4date(y, m, d):
+def _loaddata(today):
     """
-    Fetch data for the given date.
+    Parse the specified data csv file and load it into global data dictionary.
+
+    NOTE: It uses the white and black lists wrt MFTypes and MFNames, if any
+    specified in gData, to decide whether a given MF should be added to the
+    dataset or not. User can control this in the normal usage flow by either
+    passing these lists explicitly to load_data and or by setting related
+    global variables before calling load_data.
     """
-    url = MFS_BaseURL.format(d,calendar.month_name[m][:3],y)
-    fName = MFS_FNAMECSV_TMPL.format(y,m,d)
+    tFile = open(sFile)
+    curMFType = ""
+    bSkipCurMFType = False
+    typeId = -1
+    for l in tFile:
+        l = l.strip()
+        if l == '':
+            continue
+        if l[0].isalpha():
+            #print("WARN:parse_csv:Skipping:{}".format(l))
+            if l[-1] == ')':
+                curMFType = l
+                if curMFType not in gData['mfTypes']:
+                    typeId += 1
+                    gData['mfTypes'][curMFType] = []
+                    checkTypeId = gData['mfTypesId'].get(curMFType, -1)
+                    if checkTypeId != -1:
+                        if checkTypeId != typeId:
+                            input("DBUG:ParseCSV:TypeId Mismatch")
+                    gData['mfTypesId'][curMFType] = typeId
+                if gData['whiteListMFTypes'] == None:
+                    bSkipCurMFType = False
+                else:
+                    #breakpoint()
+                    fm,pm = hlpr.matches_templates(curMFType, gData['whiteListMFTypes'])
+                    if len(fm) == 0:
+                        bSkipCurMFType = True
+                    else:
+                        bSkipCurMFType = False
+            continue
+        if bSkipCurMFType:
+            continue
+        try:
+            la = l.split(';')
+            code = int(la[0])
+            name = hlpr.string_cleanup(la[1], gNameCleanupMap)
+            if (gData['whiteListMFNames'] != None):
+                fm, pm = hlpr.matches_templates(name, gData['whiteListMFNames'])
+                if len(fm) == 0:
+                    gData['skipped'].add(str([code, name]))
+                    continue
+            if (gData['blackListMFNames'] != None):
+                fm, pm = hlpr.matches_templates(name, gData['blackListMFNames'])
+                if len(fm) > 0:
+                    gData['skipped'].add(str([code, name]))
+                    continue
+            try:
+                nav  = float(la[4])
+            except:
+                nav = 0
+            date = datetime.datetime.strptime(la[7], "%d-%b-%Y")
+            date = hlpr.dateint(date.year,date.month,date.day)
+            #print(code, name, nav, date)
+            mfIndex = gData['code2index'].get(code, None)
+            if mfIndex == None:
+                mfIndex = gData['nextMFIndex']
+                gData['nextMFIndex'] += 1
+                gData['code2index'][code] = mfIndex
+                gData['index2code'][mfIndex] = code
+                gData['names'].append(name)
+                gData['mfTypes'][curMFType].append(code)
+                gData['typeId'][mfIndex] = typeId
+            else:
+                if name != gData['names'][mfIndex]:
+                    input("WARN:parse_csv:Name mismatch?:{} != {}".format(name, gData['names'][mfIndex]))
+            gData['data'][mfIndex,gData['dateIndex']] = nav
+            gData['lastSeen'][mfIndex] = date
+        except:
+            print("ERRR:parse_csv:{}".format(l))
+            print(sys.exc_info())
+    tFile.close()
+
+
+def _fetchdata(url, fName):
+    """
+    Fetch give url to specified file, and check its valid.
+    """
     print(url, fName)
     hlpr.wget_better(url, fName)
     f = open(fName)
@@ -138,6 +219,39 @@ def fetch4date(y, m, d):
     f.close()
 
 
+def _pickleok(fName):
+    fName = "{}.pickle".format(fName)
+    return os.path.exists(fName)
+
+
+def fetch4date(y, m, d):
+    """
+    Fetch data for the given date.
+    """
+    url = MFS_BaseURL.format(d,calendar.month_name[m][:3],y)
+    fName = MFS_FNAMECSV_TMPL.format(y,m,d)
+    if not _pickleok(fName):
+        _fetchdata(url, fName)
+        today = parse_csv(fName)
+        _savepickle(fName, today)
+
+
+def _savepickle(fName, today):
+    fName = "{}.pickle".format(fName)
+    f = open(fName, 'wb+')
+    pickle.dump(today, f)
+    f.close()
+
+
+def _loadpickle(fName):
+    fName = "{}.pickle".format(fName)
+    if os.path.exists(fName):
+        f = open(fName, 'rb')
+        today = pickle.load(f)
+        return True, today
+    return False, None
+
+
 def load4date(y, m, d):
     """
     Load data for the given date.
@@ -146,6 +260,7 @@ def load4date(y, m, d):
     you will have to call fillin4holidays explicitly.
     """
     fName = MFS_FNAMECSV_TMPL.format(y,m,d)
-    parse_csv(fName)
+    today = _loadpickle(fName)
+    _loaddata(today)
 
 
