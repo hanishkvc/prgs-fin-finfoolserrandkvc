a2218fa496d2a58c2e5713054256a43252926ad3
hanishkvc <hanishkvc@gmail.com>
2021-03-17 11:11:33 +0530

    Update tabcomplete wrt mf to ent switch

--- tabcomplete.py
+++ tabcomplete.py
@@ -8,7 +8,7 @@ import readline
 gData = None
 L1 = [ "load_data", "fetch_data", "lookat_data", "search_data",
         "show_plot",
-        "mftypes_list", "mftypes_members",
+        "enttypes_list", "enttypes_members",
         "procdata_ex", "plot_data",
         "loadfilters_set", "loadfilters_clear",
         "analdata_simple",
