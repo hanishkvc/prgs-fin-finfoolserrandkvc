2aedb8ed0fa83123e0ee0e3e829c6012addced70
hanishkvc <hanishkvc@gmail.com>
2021-04-11 11:46:22 +0530

    Data: Remove non data related logics
    Main program will handle these things

--- data.py
+++ data.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-# Get and work with data about MFs, indexes, stocks, or any other event/entity, etc,
-# in _BLIND_ AND _STUPID_ ways.
+# Handle the data related operations
 # HanishKVC, 2021
 # GPL
 #
@@ -26,26 +24,7 @@ import procdata
 import plot
 
 
-"""
-Usage scenario
-    fetch_data(2010, 2021)
-    load_data(2013, 20190105)
-    # explore option 1
-    procdata.infoset1_prep()
-    procdata.infoset1_result(['open elss'], ['direct'])
-    procdata.infoset1_result(['open equity large', 'open equity large mid', 'open equity flexi', 'open equity multi', 'open equity elss'], ['direct'])
-    # explore option 2
-    procdata.ops(['srel=srel(data)', 'roll3Y=roll1095(data)'])
-    search_data(['match name tokens1', 'match name tokens2'])
-    procdata.anal_simple('srel', 'srel_retpa', 'top')
-    procdata.anal_simple('roll3Y', 'roll_avg', 'top')
-    plot.data('srel', [ entCode1, entCode2 ])
-    plot.show()
-    quit()
-"""
-
 gbDEBUG = False
-FINFOOLSERRAND_BASE = None
 
 #
 # proc_days related controls
@@ -68,15 +47,6 @@ gDataKeys = ['open', 'high', 'low', 'data', 'volume']
 gDataAliases = { 'data': [ 'nav', 'close' ] }
 gDS = []
 
-
-
-def setup_paths():
-    """
-    Account for FINFOOLSERRAND_BASE env variable if set
-    """
-    global FINFOOLSERRAND_BASE
-    FINFOOLSERRAND_BASE = os.environ.get('FINFOOLSERRAND_BASE',"~/")
-    print("INFO:Main:setup_paths:", FINFOOLSERRAND_BASE)
 
 
 def modules_sync_gentdb(entDB):
@@ -493,128 +463,4 @@ def session_restore(sessionName):
     ok, gEntDB, tIgnore = hlpr.load_pickle(fName)
     modules_sync_gentdb(gEntDB)
 
-
-def input_multi(prompt="OO>", altPrompt="...", theFile=None):
-    """
-    Allow reading a single line or multiline of python block
-    either from console or from the file specified.
-
-    If user is entering a multiline python block, the program
-    will show a different prompt to make it easy for the user
-    to identify the same.
-
-    Entering a empty line or a line with a smaller indentation
-    than that used when multiline block entry started, will
-    lead to the logic getting out of the multiline input mode.
-
-    It allows user to split lists, sets, dictionary etc to be
-    set across multiple lines, provided there is ',' as the
-    last char in the inbetween lines.
-
-    It allows if-else or if-elif-else multiline blocks.
-    """
-    lines = ""
-    bMulti = False
-    bIf = False
-    lineCnt = 0
-    refStartWS = 0
-    while True:
-        if theFile == None:
-            line = input(prompt)
-        else:
-            line = theFile.readline()
-            if line == '':
-                theFile=None
-                continue
-        if prompt != altPrompt:
-            prompt = altPrompt
-        lineCnt += 1
-        lineStripped = line.strip()
-        if (lineCnt == 1):
-            lines = line
-            if (lineStripped != "") and (lineStripped[-1] in ':,\\'):
-                if lineStripped.split()[0] == 'if':
-                    bIf = True
-                bMulti = True
-                continue
-            else:
-                break
-        else:
-            if lineStripped == "":
-                break
-            curStartWS = len(line) - len(line.lstrip())
-            #print(curStartWS)
-            if (lineCnt == 2):
-                refStartWS = curStartWS
-            lines = "{}\n{}".format(lines,line)
-            if (refStartWS > curStartWS):
-                if bIf and (lineStripped.split()[0] in [ 'else:', 'elif']):
-                    continue
-                break
-    return lines
-
-
-gbREPLPrint = True
-def do_run(theFile=None):
-    """
-    Run the REPL logic of this program.
-    Read-Eval-Print Loop
-
-    NOTE: If a script file is passed to the logic, it will fall back to
-    interactive mode, once there are no more commands in the script file.
-        Script file can use quit() to exit the program automatically
-        if required.
-
-    NOTE: One can control printing of REPL, by controlling gbREPLPrint.
-    NOTE: User can suppress auto printing of individual python statements
-            entered into interactive mode by suffixing them with ';'
-    """
-    bQuit = False
-    while not bQuit:
-        bPrint = False
-        try:
-            #breakpoint()
-            cmd = input_multi(theFile=theFile)
-            if gbREPLPrint:
-                cmdStripped = cmd.strip()
-                if (cmdStripped != "") and ('\n' not in cmd) and (cmdStripped[-1] != ';'):
-                    cmd = "_THE_RESULT = {}".format(cmd)
-                    bPrint=True
-            exec(cmd,globals())
-            if bPrint and (type(_THE_RESULT) != type(None)):
-                print(_THE_RESULT)
-        except:
-            excInfo = sys.exc_info()
-            if excInfo[0] == SystemExit:
-                break
-            traceback.print_exc()
-
-
-def handle_args():
-    """
-    Logic to handle the commandline arguments
-    """
-    if sys.argv[1].endswith(".mf"):
-        print("INFO:Running ", sys.argv[1])
-        f = open(sys.argv[1])
-        do_run(f)
-    else:
-        fetch_data(sys.argv[1], sys.argv[2])
-        load_data(sys.argv[1], sys.argv[2])
-        lookat_data("OP:TOP")
-        show_plot()
-
-
-#
-# The main flow starts here
-#
-print("FinFoolsErrandKVC: A stupid exploration of multiple sets of numbers (MFs/Indexes/...) data")
-print("License: GPL")
-input("PLEASE DO NOT USE THIS PROGRAM TO MAKE ANY DECISIONS OR INFERENCES OR ...")
-
-setup()
-if len(sys.argv) > 1:
-    handle_args()
-else:
-    do_run()
 
