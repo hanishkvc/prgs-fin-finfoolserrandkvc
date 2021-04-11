ed5aae3d2d152e7f44c793e758c5e291f11a461b
hanishkvc <hanishkvc@gmail.com>
2021-04-11 21:29:30 +0530

    Stocks: Add minimal notes for the functions.

--- stocks.py
+++ stocks.py
@@ -12,6 +12,10 @@ import procedb
 
 
 def load(startDate=None, endDate=None):
+    """
+    Load data from stocks related data sources.
+    If no date range is given, then it loads data for the last 7 years.
+    """
     if startDate == None:
         endDate = time.gmtime().tm_year
         startDate = endDate - 7
@@ -19,10 +23,20 @@ def load(startDate=None, endDate=None):
 
 
 def prep():
+    """
+    Calculate some of the data required for later.
+    """
     procedb.ops(['dma50=dma50(data)', 'dma200=dma200(data)'])
 
 
 def _plot(entCodes):
+    """
+    Plot data related to the given set of entCodes.
+
+    This includes the close related
+        raw, dma50 and dma200 data as well as
+        linear regression based lines wrt 1Y, 3Y and 5Y.
+    """
     mPlot._data(['data', 'dma50', 'dma200'], entCodes)
     mPlot.linregress('data', entCodes)
 
