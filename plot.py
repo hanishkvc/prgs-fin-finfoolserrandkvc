7f01ba7a4cc4b0d2582adb10c314ce78dca7f91d
hanishkvc <hanishkvc@gmail.com>
2021-04-15 06:55:13 +0530

    Plot: Accept a axes argument
    Now the plot related functions take a optional axes argument.
    If no specific axes object is passed, it defaults to plt.gca

--- plot.py
+++ plot.py
@@ -25,7 +25,13 @@ def _entDB(entDB=None):
     return entDB
 
 
-def _data(dataKeys, entCodes, startDate=-1, endDate=-1, entDB=None):
+def _axes(axes=None):
+    if axes == None:
+        return plt.gca()
+    return axes
+
+
+def _data(dataKeys, entCodes, startDate=-1, endDate=-1, entDB=None, axes=None):
     """
     Plot specified datas for the specified entities from entDB, over the specified
     date range.
@@ -38,6 +44,7 @@ def _data(dataKeys, entCodes, startDate=-1, endDate=-1, entDB=None):
     Remember to call show func, when you want to see plots, accumulated till then.
     """
     entDB = _entDB(entDB)
+    axes = _axes(axes)
     startDateIndex, endDateIndex = entDB.daterange2index(startDate, endDate)
     x = numpy.arange(startDateIndex, endDateIndex+1)
     if type(dataKeys) == str:
@@ -57,10 +64,10 @@ def _data(dataKeys, entCodes, startDate=-1, endDate=-1, entDB=None):
             label = "{:<{cwidth}}:{:{width}}: {}".format(entCode, name, dataLabel, cwidth=giLabelCodeChopLen, width=giLabelNameChopLen)
             print("\t{}:{}".format(label, index))
             label = "{:<{cwidth}}:{:{width}}: {:16} : {}".format(entCode, name, dataKey, dataLabel, cwidth=giLabelCodeChopLen, width=giLabelNameChopLen)
-            plt.plot(x, entDB.data[dataKey][index, startDateIndex:endDateIndex+1], label=label)
+            axes.plot(x, entDB.data[dataKey][index, startDateIndex:endDateIndex+1], label=label)
 
 
-def data(dataKeys, entTypeTmpls, entNameTmpls, startDate=-1, endDate=-1, entDB=None):
+def data(dataKeys, entTypeTmpls, entNameTmpls, startDate=-1, endDate=-1, entDB=None, axes=None):
     """
     Plot specified datas for the specified entities from entDB, over the specified
     date range.
@@ -75,10 +82,10 @@ def data(dataKeys, entTypeTmpls, entNameTmpls, startDate=-1, endDate=-1, entDB=N
     """
     entDB = _entDB(entDB)
     entCodes = entDB.list_type_members(entTypeTmpls, entNameTmpls)
-    _data(dataKeys, entCodes, startDate, endDate, entDB)
+    _data(dataKeys, entCodes, startDate, endDate, entDB, axes)
 
 
-def _fit(dataKeys, entCodes, startDate=-1, endDate=-1, fitType='linregress', entDB=None):
+def _fit(dataKeys, entCodes, startDate=-1, endDate=-1, fitType='linregress', entDB=None, axes=None):
     """
     Plot a fitting line along with the data it tries to fit.
 
@@ -105,11 +112,11 @@ def _fit(dataKeys, entCodes, startDate=-1, endDate=-1, fitType='linregress', ent
             entName = entDB.meta['name'][index][:giLabelNameChopLen]
             y = entDB.data[dataKey][index][startDateIndex:endDateIndex]
             x = numpy.arange(startDateIndex, endDateIndex)
-            gFits[fitType](entCode, entName, x, y)
+            gFits[fitType](entCode, entName, x, y, axes)
 
 
 gbLineRegressPlotData = False
-def _linregress(entCode, entName, x, y):
+def _linregress(entCode, entName, x, y, axes):
     """
     Use linear regression to plot a fitting line along with the data it
     tries to fit.
@@ -117,16 +124,17 @@ def _linregress(entCode, entName, x, y):
     By default the actual data is not plotted. Set gbLineRegressPlotData,
     if you want this logic to plot data.
     """
+    axes = _axes(axes)
     numDays = len(x)
     lr = stats.linregress(x,y)
     if gbLineRegressPlotData:
         label = "{:<20}:{}:Data:{}days".format(entCode, entName, numDays)
         plt.plot(x, y, label=label)
     label = "{:<20}:{}:LinRegressLineFit:{}".format(entCode, entName, numDays)
-    plt.plot(x, x*lr.slope+lr.intercept, label=label)
+    axes.plot(x, x*lr.slope+lr.intercept, label=label)
 
 
-def linregress(dataKeys, entCodes, days=[7, '1M', '6M', '1Y', '3Y', '5Y'], entDB=None):
+def linregress(dataKeys, entCodes, days=[7, '1M', '6M', '1Y', '3Y', '5Y'], entDB=None, axes=None):
     """
     For the given dataKeys and entCodes, plot the corresponding data
     as well as curve fitting lines based on linear regression for
@@ -140,22 +148,23 @@ def linregress(dataKeys, entCodes, days=[7, '1M', '6M', '1Y', '3Y', '5Y'], entDB
         endDate = entDB.dates[endDateIndex]
         startDate = entDB.dates[endDateIndex-d]
         if entDB.datesD.get(startDate, -1) >= 0:
-            _fit(dataKeys, entCodes, startDate, endDate, 'linregress', entDB)
+            _fit(dataKeys, entCodes, startDate, endDate, 'linregress', entDB, axes)
 
 
-def _spline(entCode, entName, x, y):
+def _spline(entCode, entName, x, y, axes):
     """
     Use linear regression to plot a fitting line along with the data it
     tries to fit.
     """
+    axes = _axes(axes)
     numDays = len(x)
     spl = scipy.interpolate.splrep(x,y, k=3)
     label = "{}:{}:Data:{}days".format(entCode, entName, numDays)
-    plt.plot(x, y*1.01, label=label)
+    axes.plot(x, y*1.01, label=label)
     label = "{}:{}:SplineFit".format(entCode, entName)
     x = numpy.append(x, numpy.arange(x[-1],x[-1]+2))
     ySpl = scipy.interpolate.splev(x, spl)
-    plt.plot(x, ySpl, label=label)
+    axes.plot(x, ySpl, label=label)
 
 
 gFits = {
@@ -164,15 +173,16 @@ gFits = {
     }
 
 
-def _show(entDB):
+def _show(entDB, axes=None):
     """
     Show the data plotted till now.
     """
-    leg = plt.legend()
+    axes = _axes(axes)
+    leg = axes.legend()
     plt.setp(leg.texts, family='monospace')
     for line in leg.get_lines():
         line.set_linewidth(8)
-    plt.grid(True)
+    axes.grid(True)
     startDateIndex, endDateIndex = entDB.daterange2index(-1,-1)
     curDates = entDB.dates[startDateIndex:endDateIndex+1].astype('int')
     """
@@ -182,16 +192,15 @@ def _show(entDB):
     xTickLabels = numpy.array(curDates)[xTicks]
     plt.xticks(xTicks, xTickLabels, rotation='vertical')
     """
-    ax = plt.gca()
-    ax.xaxis.set_major_formatter(matplotlib.ticker.IndexFormatter(curDates))
-    plt.show()
+    axes.xaxis.set_major_formatter(matplotlib.ticker.IndexFormatter(curDates))
 
 
-def show(entDB=None):
+def show(entDB=None, axes=None):
     """
     Show the data plotted till now.
     """
     entDB = _entDB(entDB)
-    _show(entDB)
+    _show(entDB, axes)
+    plt.show()
 
 
